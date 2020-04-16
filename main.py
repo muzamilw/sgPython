ca65c378b041076d451522e88ba29750af4ee087
Muzammil Waheed <muzamilw@hotmail.com>
2020-04-16 16:06:42 +0500

    kivy ui in progress

--- main.py
+++ main.py
@@ -27,6 +27,16 @@ import customFunctions as cf
 import apiWrappers as apiW
 import botLogic
 
+import kivy
+kivy.require('1.0.7')
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.app import App
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+import os
+from connected import Connected
+
 
 class GlobalVars:
     pass
@@ -209,7 +219,7 @@ api = IGLogin('nevillekmiec','!_LKvXc1')
 
 print(api.authenticated_user_id)
 
-botLogic.RunBot(gVars,api,Client)
+#botLogic.RunBot(gVars,api,Client)
 
 
 ########################################################
@@ -255,4 +265,51 @@ with open('glob.Vars', 'wb') as gVarFile:
 
 
 
+class Login(Screen):
+    def do_login(self, loginText, passwordText):
+        app = App.get_running_app()
 
+        app.username = loginText
+        app.password = passwordText
+
+        gVars.loginResult = cf.AppLogin(loginText,passwordText,'123',gVars)
+
+        print(gVars.loginResult)
+
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'connected'
+
+        app.config.read(app.get_application_config())
+        app.config.write()
+
+    def resetForm(self):
+        self.ids['login'].text = ""
+        self.ids['password'].text = ""
+
+class LoginApp(App):
+    username = StringProperty(None)
+    password = StringProperty(None)
+
+    def build(self):
+        manager = ScreenManager()
+
+        manager.add_widget(Login(name='login'))
+        manager.add_widget(Connected(name='connected'))
+
+        return manager
+
+    def get_application_config(self):
+        if(not self.username):
+            return super(LoginApp, self).get_application_config()
+
+        conf_directory = self.user_data_dir + '/' + self.username
+
+        if(not os.path.exists(conf_directory)):
+            os.makedirs(conf_directory)
+
+        return super(LoginApp, self).get_application_config(
+            '%s/config.cfg' % (conf_directory)
+        )
+
+if __name__ == '__main__':
+    LoginApp().run()
