2d4c160b23e565681712b26aff270740554da90a
Muzammil Waheed <muzamilw@hotmail.com>
2020-04-25 17:05:43 +0500

    Update alert.py

--- alert.py
+++ alert.py
@@ -10,7 +10,7 @@ from kivy.uix.popup import Popup
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.core.window import Window
+from kivy.core.window import Window 
 
 class Alert(Popup):
 
